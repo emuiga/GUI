4a6f89685f713ec841f2eaa8db60993ae64d18ef
Steve <muigastephen14@gmail.com>
2022-08-09 08:20:54 +0300

    gui

---
+++ GUI.java
@@ -0,0 +1,51 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GUI implements ActionListener, java.awt.event.ActionListener {
+
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI() {
+
+        frame = new JFrame();
+
+        JButton button = new JButton("Click Me");
+        button.addActionListener(this); //add ActionListener to implement ActionListener
+
+        label = new JLabel("Number of clicks: 0");
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("My GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main (String[] args) {
+       new GUI(); 
+    }
+
+    //ActionListener looks for actionPerformed method
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        //actionPerformed tied to button object, it listens and runs code inside here
+        label.setText("Number of clicks: " + count);
+    }
+}
